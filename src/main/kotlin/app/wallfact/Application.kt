90d54adefe556235ddc1a4024175ce4bd41d89f0
mnpilpil <uorriuart@yandex.ru>
2021-08-15 13:04:08 +0300

    Add mongodb

--- src/main/kotlin/app/wallfact/Application.kt
+++ src/main/kotlin/app/wallfact/Application.kt
@@ -1,10 +1,16 @@
 package app.wallfact
 
 import app.wallfact.integration.pixabay.impl.PixabayRepo
+import app.wallfact.model.Fact
 import app.wallfact.plugins.configureRouting
+import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.runBlocking
+import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main() {
     runBlocking {
@@ -12,7 +18,17 @@ fun main() {
         println(response.content.readUTF8Line(Int.MAX_VALUE))
     }
 
+    val client = KMongo.createClient().coroutine
+    val database = client.getDatabase("wallfactdb")
+    val col = database.getCollection<Fact>()
+
+    runBlocking {
+        val facts: List<Fact> = col.find().toList()
+        println(facts)
+    }
+
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
     }.start(wait = true)
 }
+
